b30c28e955a0c55df75b225bd6081c46ececb083
lucabaldelli <luca.baldelli@gmail.com>
2015-09-24 19:56:08 +0100

    reset and resize mazes

--- app/controllers/mazes_controller.rb
+++ app/controllers/mazes_controller.rb
@@ -3,9 +3,13 @@ class MazesController < ApplicationController
     respond_to do |format|
       format.html { render :random }
       format.json do
-        @maze = Maze.new size: 40
+        @maze = Maze.new size: size
         render json: @maze.to_json
       end
     end
+  end
+
+  def size
+    params.require(:size).to_i
   end
 end
